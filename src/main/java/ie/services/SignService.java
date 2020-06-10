a79d9bfe42fd5b73b91da2748a98d2707aea9f30
fatemeh-al <fatemehalagheband@gmail.com>
2020-06-10 12:13:05 +0430

    googleLogin changed

--- src/main/java/ie/services/SignService.java
+++ src/main/java/ie/services/SignService.java
@@ -1,5 +1,7 @@
 package ie.services;
 
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import ie.logic.Loghme;
 import ie.logic.Resturant;
 import ie.services.responses.JwtResponse;
@@ -9,8 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @RestController
 public class SignService {
@@ -40,13 +46,42 @@ public class SignService {
 
     @RequestMapping(value = "/googleLogin", method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<JwtResponse> loginUserWithGoogle(@RequestParam(value = "email") String email)
+    public ResponseEntity<JwtResponse> loginUserWithGoogle(@RequestParam(value = "email") String email,
+                                                           @RequestParam(value="idToken") String idTokenString)
     {
-        String jwtToken = Loghme.getInstance().loginWithGoogle(email);
-        if(jwtToken == null)
-            return new ResponseEntity<JwtResponse>(new JwtResponse(jwtToken), HttpStatus.CONFLICT);
-        else
-            return new ResponseEntity<JwtResponse>(new JwtResponse(jwtToken), HttpStatus.ACCEPTED);
+        JwtResponse fakeResponse = null;
+        try{
+            System.out.println(1);
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+                    .setAudience(Collections.singletonList("822539019781-se58ntb5ml53t7es2odus494vkifdtso.apps.googleusercontent.com"))//CLIENT_ID
+                    .build();
+            System.out.println(2);
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            System.out.println(3);
+            if(idToken != null){
+                System.out.println(4);
+                Payload payload = idToken.getPayload();
+                String emailPayload = payload.getEmail();
+                boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+                String jwtToken = Loghme.getInstance().loginWithGoogle(emailPayload);
+                System.out.println(5);
+                if(jwtToken == null || !emailVerified){
+                    System.out.println(6);
+                    return new ResponseEntity<JwtResponse>(fakeResponse, HttpStatus.CONFLICT);
+                }
+                else {
+                    System.out.println(7);
+                    return new ResponseEntity<JwtResponse>(new JwtResponse(jwtToken), HttpStatus.ACCEPTED);
+                }
+            }
+            System.out.println(8);
+            return new ResponseEntity<JwtResponse>(fakeResponse, HttpStatus.CONFLICT);
+        }catch (Exception e){
+            System.out.println(9);
+            System.out.println("Exception in googleLogin function");
+            System.out.println(e);
+            return new ResponseEntity<JwtResponse>(fakeResponse, HttpStatus.CONFLICT);
+        }
     }
 
     private ResponseEntity<StatusCode> resultDecoder(Loghme.Status status){
